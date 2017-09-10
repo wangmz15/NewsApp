5ef1560376e53345d0c8f12d542021b3b414437a
WangShicheng <wscthu2015@gmail.com>
2017-09-11 00:12:23 +0800

    分享功能目前只有新浪微博和QQ以及QQ空间，微信分享功能还在审核中。 语音播报还没有实现解析html文件中的正文
    Signed-off-by: WangShicheng <wscthu2015@gmail.com>

---
+++ app/src/main/java/com/ihandy/a2014011446/biz/NewsItemBiz.java
@@ -0,0 +1,108 @@
+package com.ihandy.a2014011446.biz;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.ihandy.a2014011446.bean.NewsItem;
+import com.ihandy.a2014011446.dao.NewsItemDao;
+import com.ihandy.a2014011446.utils.FileUtils;
+import com.ihandy.a2014011446.utils.HttpUtils;
+import com.ihandy.a2014011446.utils.NewsAPIUtils;
+import com.ihandy.a2014011446.utils.StringUtils;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * 处理新闻的业务逻辑类
+ */
+public class NewsItemBiz {
+
+    private NewsItemDao mNewsItemDao;
+    private FileUtils fileUtils;
+
+    public NewsItemBiz(Context context) {
+        mNewsItemDao = new NewsItemDao(context);
+        fileUtils = new FileUtils(context);
+    }
+    public NewsItemDao getmNewsItemDao(){
+        return mNewsItemDao;
+    }
+
+
+    /**
+     * 获取新闻项的数据库缓存
+     * @param newsType  类型
+     * @param currentPage   当前页
+     * @return  新闻项列表缓存
+     * @throws SQLException
+     */
+    public List<NewsItem> getNewsItemCache(String newsType,int currentPage) throws SQLException {
+        return mNewsItemDao.getCache(newsType);
+    }
+
+    /**
+     * 根据新闻类型和页码得到新闻列表
+     * @param newsType      新闻URL类型
+     * @param currentPage   页码
+     * @return              新闻列表
+     * @throws Exception
+     */
+    public List<NewsItem> getNewsItems(String newsType,int currentPage) throws Exception {
+
+        String url = NewsAPIUtils.getNewsUrl(newsType, currentPage);
+
+        String jsonStr = null;
+        //如果服务器未返回数据,则返回数据库中的数据
+        try {
+            jsonStr = HttpUtils.doGet(url);
+        }catch (Exception ex){
+            return getNewsItemCache(newsType,currentPage);
+        }
+        Log.i("GETITEM", "getNewsItems: " + url);
+        jsonStr = StringUtils.replaceBlankAndSpace(jsonStr);
+
+        //jsonStr = StringUtils.replaceNme(jsonStr);
+
+        String PATTERN = "\"list\":(\\[.*\\])";
+        Pattern p =Pattern.compile(PATTERN);
+        Matcher m = p.matcher(jsonStr);
+        String jsonList = null;
+        if(m.find()) jsonList = m.group(1);
+        else return null;
+        Log.i(getClass().getName(), "jsonList: " + jsonList);
+
+        jsonList = StringUtils.getRightJsonSyntax(jsonList);
+        Log.i(getClass().getName(), "rightJsonList: " + jsonList);
+
+
+        List<NewsItem> newsItems = new ArrayList<NewsItem>();
+        try {
+            newsItems = new Gson().fromJson(jsonList, new TypeToken<List<NewsItem>>() {}.getType());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for(NewsItem newsItem : newsItems) {
+            //Log.i(newsItem.getTitle(),"newsItemInfo"+newsItem.getSourceUrl());
+            Log.i("info",currentPage+newsItem.getTitle());
+            mNewsItemDao.createOrUpdate(newsItem);
+        }
+
+
+        return newsItems;
+
+    }
+    /**
+     * 清除缓存数据库
+     */
+    public void clearCache(){
+        mNewsItemDao.deleteAll();
+        fileUtils.deleteFile();
+    }
+}
---
+++ app/src/main/java/com/ihandy/a2014011446/dao/FavoriteItemDao.java
@@ -0,0 +1,42 @@
+package com.ihandy.a2014011446.dao;
+
+import android.content.Context;
+
+import com.ihandy.a2014011446.bean.FavoriteItem;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+import com.j256.ormlite.stmt.DeleteBuilder;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by wuhanghang on 16-9-3.
+ */
+
+public class FavoriteItemDao {
+    private RuntimeExceptionDao<FavoriteItem,Integer> mFavoriteItemDao;
+    private DataBaseHelper mDataBaseHelpter;
+
+    public FavoriteItemDao(Context context) {
+        mDataBaseHelpter = DataBaseHelper.getHelper(context);
+        this.mFavoriteItemDao = mDataBaseHelpter.getFavoriteItemRuntimeDao();
+    }
+
+    public void createOrUpdate(FavoriteItem favoriteItem){
+        mFavoriteItemDao.createOrUpdate(favoriteItem);
+    }
+
+    public List<FavoriteItem> queryAll(){
+        List<FavoriteItem> favorites = mFavoriteItemDao.queryForAll();
+        return favorites;
+    }
+    public int deleteByNewsId(String newsId) throws SQLException {
+        DeleteBuilder<FavoriteItem, Integer> deleteBuilder = mFavoriteItemDao.deleteBuilder();
+        deleteBuilder.where().eq("news_ID",newsId);
+        return deleteBuilder.delete();
+    }
+    public boolean searchIsExistByNewsId(String newsId) throws SQLException {
+        List<FavoriteItem> favoriteItems = mFavoriteItemDao.queryBuilder().where().eq("news_ID",newsId).query();
+        return (favoriteItems.size() > 0);
+    }
+}
